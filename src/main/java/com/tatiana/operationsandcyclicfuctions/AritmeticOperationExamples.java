02a9ded5527daf499307c4c4d7d24c8ab235adb3
Tatiannna9 <vizdoagatatiana95@gmail.com>
2023-05-29 22:50:28 +0300

    Added the implementation of Java Operations, Cyclic Functions and Control Structures.

---
+++ src/main/java/com/tatiana/operationsandcyclicfuctions/AritmeticOperationExamples.java
@@ -0,0 +1,35 @@
+package com.tatiana.operationsandcyclicfuctions;
+
+public class AritmeticOperationExamples {
+    public static void main(String[] args) {
+        int a = 43;
+        int c = 3;
+
+        //Sum
+        int sum = a + c;
+        System.out.println("Suma:" + sum);
+        System.out.println("Suma:" + (a + c));
+
+        //Substruction
+        int sub = a - c;
+        System.out.println("Rezultatul:" + sub);
+        System.out.println("Rezultatul:" + (a-c));
+
+        //Multiplication
+        int result = a * c;
+        System.out.println("Inmultirea:" + result);
+        System.out.println("Inmultirea:" + (a*c));
+
+        //Devision
+        result = a / c;
+        System.out.println("Impartirea:" + result);
+        System.out.println("Impartirea:" + (a/c));
+
+        //Remainder of devision
+        result = a % c;
+        System.out.println("Impartirea:" + result);
+        System.out.println("Impartirea:" + (a%c));
+
+    }
+
+}
